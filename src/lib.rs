fac9df54cde3a93ead90bfbc6e2a7070356ec2c4
quest4ione <196376222+quest4ione@users.noreply.github.com>
2025-06-25 21:06:02 +0200

    feat: add dynamic memory trait (completely untested again)

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,71 @@
 use num::PrimInt;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct Subleq<T: PrimInt, const MEM_SIZE: usize> {
-    pub mem: [T; MEM_SIZE],
-    pub curr_instruction: usize,
+pub trait Memory<T: PrimInt>: Default {
+    fn get(&self, index: usize) -> T;
+    fn set(&mut self, index: usize, value: T);
 }
 
-impl<T: PrimInt, const MEM_SIZE: usize> Default for Subleq<T, MEM_SIZE> {
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Subleq<T: PrimInt, M: Memory<T>> {
+    pub mem: M,
+    pub curr_instruction: usize,
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<T: PrimInt, M: Memory<T>> Default for Subleq<T, M> {
     fn default() -> Self {
         Self {
-            mem: [T::zero(); MEM_SIZE],
+            mem: M::default(),
             curr_instruction: 0,
+            _marker: std::marker::PhantomData,
         }
     }
 }
 
-impl<T: PrimInt, const MEM_SIZE: usize> Subleq<T, MEM_SIZE> {
-    const _ASSERT: () = assert!(MEM_SIZE != 0);
-
+impl<T: PrimInt, M: Memory<T>> Subleq<T, M> {
     pub fn new() -> Self {
-        Default::default()
+        Self::default()
     }
 
-    pub fn with_memory(mem: [T; MEM_SIZE]) -> Self {
+    pub fn with_memory(mem: M) -> Self {
         Self {
             mem,
-            ..Default::default()
+            curr_instruction: 0,
+            _marker: std::marker::PhantomData,
         }
-    }
-
-    pub fn get(&self, index: usize) -> T {
-        self.mem[index % MEM_SIZE]
-    }
-
-    pub fn set(&mut self, index: usize, value: T) {
-        self.mem[index % MEM_SIZE] = value;
     }
 
     pub fn step(&mut self) {
         let (a_addr, b_addr, c_addr) = (
-            self.get(self.curr_instruction)
+            self.mem
+                .get(self.curr_instruction)
                 .to_usize()
                 .expect("cant convert number into usize address"),
-            self.get(self.curr_instruction.wrapping_add(1))
+            self.mem
+                .get(self.curr_instruction.wrapping_add(1))
                 .to_usize()
                 .expect("cant convert numbner into usize address"),
-            self.get(self.curr_instruction.wrapping_add(2))
+            self.mem
+                .get(self.curr_instruction.wrapping_add(2))
                 .to_usize()
                 .expect("cant convert number into usize address"),
         );
 
-        let (a, b, c) = (self.get(a_addr), self.get(b_addr), self.get(c_addr));
+        let (a, b, c) = (
+            self.mem.get(a_addr),
+            self.mem.get(b_addr),
+            self.mem.get(c_addr),
+        );
 
         let result = b - a;
-        self.set(b_addr, result);
+        self.mem.set(b_addr, result);
 
         if result <= T::zero() {
             self.curr_instruction = c
                 .to_usize()
                 .expect("cant convert number into usize address");
         } else {
-            self.curr_instruction = self.curr_instruction.wrapping_add(3) % MEM_SIZE
+            self.curr_instruction = self.curr_instruction.wrapping_add(3)
         }
     }
 }
