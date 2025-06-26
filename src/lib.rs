e5a850676a7b66cb33d81c44fe49f5b368aeba2e
quest4ione <196376222+quest4ione@users.noreply.github.com>
2025-06-26 14:55:23 +0200

    feat: add errors for value conversion and address ranges

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 use num::PrimInt;
 
+#[non_exhaustive]
+pub enum Error {
+    CantConvertValueToUsize,
+    AddressOutOfRange(usize),
+}
+
 pub trait Memory<T: PrimInt>: Default {
-    fn get(&self, index: usize) -> T;
-    fn set(&mut self, index: usize, value: T);
+    fn get(&self, index: usize) -> Result<T, Error>;
+    fn set(&mut self, index: usize, value: T) -> Result<(), Error>;
 }
 
 pub struct LinearMemory<T: PrimInt, const SIZE: usize>([T; SIZE]);
@@ -14,12 +20,21 @@ impl<T: PrimInt, const SIZE: usize> Default for LinearMemory<T, SIZE> {
 }
 
 impl<T: PrimInt, const SIZE: usize> Memory<T> for LinearMemory<T, SIZE> {
-    fn get(&self, index: usize) -> T {
-        self.0[index % SIZE]
+    fn get(&self, address: usize) -> Result<T, Error> {
+        self.0
+            .get(address)
+            .ok_or(Error::AddressOutOfRange(address))
+            .copied()
     }
 
-    fn set(&mut self, index: usize, value: T) {
-        self.0[index % SIZE] = value;
+    fn set(&mut self, address: usize, value: T) -> Result<(), Error> {
+        let reference = self
+            .0
+            .get_mut(address)
+            .ok_or(Error::AddressOutOfRange(address))?;
+
+        *reference = value;
+        Ok(())
     }
 }
 
@@ -53,31 +68,32 @@ impl<T: PrimInt, M: Memory<T>> Subleq<T, M> {
         }
     }
 
-    pub fn step(&mut self) {
-        let (a_addr, b_addr, c_addr) = (
+    pub fn step(&mut self) -> Result<(), Error> {
+        let (a, b, c) = (
             self.mem
-                .get(self.curr_instruction)
+                .get(self.curr_instruction)?
                 .to_usize()
-                .expect("cant convert number into usize address"),
+                .ok_or(Error::CantConvertValueToUsize)?,
             self.mem
-                .get(self.curr_instruction.wrapping_add(1))
+                .get(self.curr_instruction.wrapping_add(1))?
                 .to_usize()
-                .expect("cant convert number into usize address"),
+                .ok_or(Error::CantConvertValueToUsize)?,
             self.mem
-                .get(self.curr_instruction.wrapping_add(2))
+                .get(self.curr_instruction.wrapping_add(2))?
                 .to_usize()
-                .expect("cant convert number into usize address"),
+                .ok_or(Error::CantConvertValueToUsize)?,
         );
 
-        let (a, b) = (self.mem.get(a_addr), self.mem.get(b_addr));
+        let (a_value, b_value) = (self.mem.get(a)?, self.mem.get(b)?);
 
-        let result = b - a;
-        self.mem.set(b_addr, result);
+        let result = b_value - a_value;
+        self.mem.set(b, result)?;
 
         if result <= T::zero() {
-            self.curr_instruction = c_addr;
+            self.curr_instruction = c;
         } else {
             self.curr_instruction = self.curr_instruction.wrapping_add(3)
         }
+        Ok(())
     }
 }
