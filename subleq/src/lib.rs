5bcf07cb87382bb16563b298d5ace54b835956f5
quest4ione <196376222+quest4ione@users.noreply.github.com>
2025-06-27 22:01:16 +0200

    refactor: seperate error and memory into their own modules

--- subleq/src/lib.rs
+++ subleq/src/lib.rs
@@ -1,48 +1,9 @@
+mod error;
+pub mod memory;
+pub use error::Error;
+pub use memory::Memory;
+
 use num::{Signed, Zero, cast::AsPrimitive};
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-#[non_exhaustive]
-pub enum Error {
-    #[error("address `{0}` is out of range for memory")]
-    AddressOutOfRange(usize),
-    #[error("immutable memory address `{0}`")]
-    ImmutableAddress(usize),
-    #[error("custom error: {0}")]
-    Custom(#[source] Box<dyn std::error::Error>),
-}
-
-pub trait Memory<T: Signed + Zero + AsPrimitive<usize>>: Default {
-    fn get(&self, index: usize) -> Result<T, Error>;
-    fn set(&mut self, index: usize, value: T) -> Result<(), Error>;
-}
-
-pub struct LinearMemory<T: Signed + Zero + AsPrimitive<usize>, const SIZE: usize>([T; SIZE]);
-
-impl<T: Signed + Zero + AsPrimitive<usize>, const SIZE: usize> Default for LinearMemory<T, SIZE> {
-    fn default() -> Self {
-        Self([T::zero(); SIZE])
-    }
-}
-
-impl<T: Signed + Zero + AsPrimitive<usize>, const SIZE: usize> Memory<T> for LinearMemory<T, SIZE> {
-    fn get(&self, address: usize) -> Result<T, Error> {
-        self.0
-            .get(address)
-            .ok_or(Error::AddressOutOfRange(address))
-            .copied()
-    }
-
-    fn set(&mut self, address: usize, value: T) -> Result<(), Error> {
-        let reference = self
-            .0
-            .get_mut(address)
-            .ok_or(Error::AddressOutOfRange(address))?;
-
-        *reference = value;
-        Ok(())
-    }
-}
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Subleq<T: Signed + Zero + AsPrimitive<usize>, M: Memory<T>> {
